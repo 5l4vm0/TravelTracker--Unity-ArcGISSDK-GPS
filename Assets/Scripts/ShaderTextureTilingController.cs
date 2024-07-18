18d40d6ad7609096d23ec1ced749b6efd2d7087f
hsuanchao1 <80270750+hsuanchao1@users.noreply.github.com>
2024-07-18 22:55:01 +0100

    Loop through all visible area to create tiles

--- Assets/Scripts/ShaderTextureTilingController.cs
+++ Assets/Scripts/ShaderTextureTilingController.cs
@@ -36,15 +36,14 @@ public class ShaderTextureTilingController : MonoBehaviour
     public ValueTuple<int, int> CalculateTileNumber(Vector3 EndPoint)
     {
         int tileX = Mathf.FloorToInt((EndPoint.x - BasedRefBottomLeftPos.x) / 3000);
-        int tileY = Mathf.FloorToInt((EndPoint.z - BasedRefBottomLeftPos.z) / 3000); 
-        Debug.Log("EndPoint: " + EndPoint + "BasedRefPos: " + BasedRefBottomLeftPos);
-        Debug.Log("tileX: "+tileX+ "tileY: "+tileY);
+        int tileY = Mathf.FloorToInt((EndPoint.z - BasedRefBottomLeftPos.z) / 3000);
         return (tileX, tileY);
     }
 
-    public void AddShaderTexture(float TileNumberX, float TileNumberY)
+    public void AddShaderTexture(int TileNumberX, int TileNumberY)
     {
-        if(tiles.ContainsKey(new Vector2(TileNumberX,TileNumberY)))
+        
+        if (tiles.ContainsKey(new Vector2(TileNumberX,TileNumberY)))
         {
             tiles[new Vector2(TileNumberX, TileNumberY)] = true;
         }
@@ -52,6 +51,18 @@ public class ShaderTextureTilingController : MonoBehaviour
         {
             GameObject newTile = Instantiate(ShaderTextureTilePrefab, new Vector3(BasedRefBottomLeftPos.x + 1500 + 3000 * TileNumberX, 25, BasedRefBottomLeftPos.z + 1500 + 3000 * TileNumberY), new Quaternion(0, 0, 0, 0), this.transform);
             tiles.Add(new Vector2(TileNumberX, TileNumberY), true);
+        }
+    }
+
+    public void loopThroughViewport(int CentreTileNumberX, int CentreTileNumberY)
+    {
+        //Get centre tile number and use BottomRight - BottomLeft/3000 to know how many tiles are visble, divide into 2 so we know how many for each side(left and right), final +1 is for forloop to loop through 
+        for (int x = CentreTileNumberX - Mathf.RoundToInt((CameraMovement.Instance.BottomRight.x - CameraMovement.Instance.BottomLeft.x) / 3000 / 2); x <CentreTileNumberX+ Mathf.RoundToInt((CameraMovement.Instance.BottomRight.x - CameraMovement.Instance.BottomLeft.x) / 3000 / 2)+1; x++)
+        {
+            for (int y = CentreTileNumberY - Mathf.RoundToInt((CameraMovement.Instance.TopLeft.z - CameraMovement.Instance.BottomLeft.z) / 3000 / 2); y < CentreTileNumberY + Mathf.RoundToInt((CameraMovement.Instance.TopLeft.z - CameraMovement.Instance.BottomLeft.z) / 3000 / 2)+1; y++)
+            {
+                AddShaderTexture(x, y);
+            }
         }
     }
 
