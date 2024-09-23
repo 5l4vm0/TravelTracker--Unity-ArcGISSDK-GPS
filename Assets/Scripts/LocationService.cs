44bc977a8bdbaca3682ebafb24d2d50d10a0e147
hsuanchao1 <123881809+5l4vm0@users.noreply.github.com>
2024-09-23 20:42:10 +0100

    Code clean up + fixed 15sec bug!

--- Assets/Scripts/LocationService.cs
+++ Assets/Scripts/LocationService.cs
@@ -16,7 +16,6 @@ public class LocationService : MonoBehaviour
     [SerializeField] private ArcGISPoint _gpsPosition;
     [SerializeField] private ArcGISMapComponent _mapRef;
     private List<ArcGISPoint> _visitedPosList = new List<ArcGISPoint>();
-    private float _elapsedTime;
     private StringBuilder _allVisitedPos;
     public List<ArcGISPoint> SavedPos;
     [SerializeField] private GisPosToPixel _gisPostToPixel;
@@ -26,12 +25,14 @@ public class LocationService : MonoBehaviour
     private Vector2 _lastPointInUV;
     public bool CamInCentre = true;
     [SerializeField] private ButtonBehaviour _reCentreButton;
-    [SerializeField] private Image testUIGPS;
+    [SerializeField] private Image _testUIGPS;
     [SerializeField] private Sprite[] _globeImage;
-    [SerializeField] private Image testUIWifi;
-    private bool isFirstTimeReload = true;
+    [SerializeField] private Image _testUIWifi;
+    private bool _isFirstTimeReload = true;
     private bool _isFirstGettingGPS = true;
-    private DateTime _lastDataTimestamp;
+    private DateTime _lastGotGPSTime;
+    private DateTime _lastLocationSavedTime;
+    private DateTime _lastCheckGPSTime;
 
     //Singleton
     public static LocationService Instance;
@@ -90,97 +91,62 @@ public class LocationService : MonoBehaviour
         UnityEngine.Input.location.Start(25f, 0.1f); //Start(float desiredAccuracyInMeters, float updateDistanceInMeters);
         // desiredAccuracyInMeters = The service accuracy you want to use, in meters. This determines the accuracy of the device's last location coordinates.
         // updateDistanceInMeters = The minimum distance, in meters, that the device must move laterally before Unity updates location
-
-        // Wait until service initializes
-        int maxWait = 15;
-        while (UnityEngine.Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
-        {
-            yield return new WaitForSecondsRealtime(1);
-            maxWait--;
-        }
-
-        // Editor has a bug which doesn't set the service status to Initializing. So extra wait in Editor.
-#if UNITY_EDITOR
-        int editorMaxWait = 15;
-        while (UnityEngine.Input.location.status == LocationServiceStatus.Stopped && editorMaxWait > 0)
-        {
-            yield return new WaitForSecondsRealtime(1);
-            editorMaxWait--;
-        }
-#endif
-        // Service didn't initialize in 15 seconds
-        if (maxWait < 1)
-        {
-            // TODO Failure
-            Debug.LogFormat("Timed out");
-            yield break;
-        }
-
-        // Connection has failed
-        if (UnityEngine.Input.location.status != LocationServiceStatus.Running)
-        {
-            // TODO Failure
-            Debug.LogFormat("Unable to determine device location. Failed with status {0}", UnityEngine.Input.location.status);
-            testUIGPS.sprite = _globeImage[0];
-
-            //Try to start location service again 
-            UnityEngine.Input.location.Stop();
-            UnityEngine.Input.location.Start(25f, 0.1f);
-        }
-
-        while (true)
-        {
-            CheckInternet();
-            if (UnityEngine.Input.location.status == LocationServiceStatus.Running)
-            {
-                
-                GetGPS();
-            }
-            yield return new WaitForSecondsRealtime(3);
-        }
-
-
+        yield return null;
     }
-
-    // Stop service if there is no need to query location updates continuously
-    //UnityEngine.Input.location.Stop();
 
 
     private void Update()
     {
-        _elapsedTime += Time.deltaTime;
         
+        CheckInternet();
 
-        if(UnityEngine.Input.location.status == LocationServiceStatus.Running && _elapsedTime >= 10)
+        if (UnityEngine.Input.location.status == LocationServiceStatus.Stopped || UnityEngine.Input.location.status == LocationServiceStatus.Failed)
         {
-            // This foreach loop just to print allVisitedPos in editor
-            foreach (ArcGISPoint point in _visitedPosList)
-            {
-                _allVisitedPos.Append("pointX"+point.X+" pointY"+point.Y+"/");
-            }
-           // Debug.Log("visited positions" + _allVisitedPos.ToString());
-            _allVisitedPos.Clear();
-
-            _elapsedTime = 0;
-            SaveSystem.SavePositions(_visitedPosList);
+            RestartLocationService();
         }
 
+        if (UnityEngine.Input.location.status == LocationServiceStatus.Running && (DateTime.UtcNow -_lastLocationSavedTime).TotalSeconds > 10)
+        {
+            SaveLocationsToFile();
+        }
+
+        if (UnityEngine.Input.location.status == LocationServiceStatus.Running && (DateTime.UtcNow - _lastCheckGPSTime).TotalSeconds > 3)
+        {
+            GetGPS();
+        }
     }
+
+    private void SaveLocationsToFile()
+    {
+        // This foreach loop just to print allVisitedPos in editor
+        foreach (ArcGISPoint point in _visitedPosList)
+        {
+            _allVisitedPos.Append("pointX" + point.X + " pointY" + point.Y + "/");
+        }
+        // Debug.Log("visited positions" + _allVisitedPos.ToString());
+        _allVisitedPos.Clear();
+
+        SaveSystem.SavePositions(_visitedPosList);
+        _lastLocationSavedTime = DateTime.UtcNow;
+    }
+
+    private void RestartLocationService()
+    {
+        _testUIGPS.sprite = _globeImage[0];
+
+        //Try to start location service again 
+        UnityEngine.Input.location.Stop();
+        UnityEngine.Input.location.Start(25f, 0.1f);
+    }
+
     private void GetGPS()
     {
-        if ((_gpsPosition.X != UnityEngine.Input.location.lastData.latitude || _gpsPosition.Y != UnityEngine.Input.location.lastData.longitude)) //GPS is working
+        //GPS is working
+        if ((_gpsPosition.X != UnityEngine.Input.location.lastData.latitude || _gpsPosition.Y != UnityEngine.Input.location.lastData.longitude))
         {
-            testUIGPS.sprite = _globeImage[1];
+            //Set GPS Icon to coloured
+            _testUIGPS.sprite = _globeImage[1];
             
-
-            //Debug.LogFormat("Location service live. status {0}", UnityEngine.Input.location.status);
-            // Access granted and location value could be retrieved
-            //Debug.LogFormat("Location: "
-            //    + UnityEngine.Input.location.lastData.latitude + " "
-            //    + UnityEngine.Input.location.lastData.longitude + " "
-            //    + UnityEngine.Input.location.lastData.altitude + " "
-            //    + UnityEngine.Input.location.lastData.horizontalAccuracy + " "
-            //    + UnityEngine.Input.location.lastData.timestamp);
 
             if (_isFirstGettingGPS)
             {
@@ -215,6 +181,7 @@ public class LocationService : MonoBehaviour
             if (!_mapRef.HasSpatialReference())
             {
                 Debug.Log("no spatial refernece");
+                //TODO: Reinitialise map
                 //_playerDotRef.gameObject.transform.position = new Vector3(_mapRef.GeographicToEngine(_gpsPosition).x, _mapRef.GeographicToEngine(_gpsPosition).y, 0.1f);
             }
             else
@@ -222,16 +189,21 @@ public class LocationService : MonoBehaviour
                 _playerDotRef.Position = new ArcGISPoint(_gpsPosition.X, _gpsPosition.Y, 26, _gpsPosition.SpatialReference);
             }
 
-            _gisPostToPixel = ShaderTextureTilingController.Instance.tiles[new Vector2(CameraMovement.Instance.GetCameraCentralTile(_mapRef.GeographicToEngine(_cameraRef.Position)).Item1, CameraMovement.Instance.GetCameraCentralTile(_mapRef.GeographicToEngine(_cameraRef.Position)).Item2)].transform.GetChild(0).GetComponent<GisPosToPixel>();
+            Vector2 cameraInTextureTileNumber = new Vector2(
+                CameraMovement.Instance.GetCameraCentralTile(_mapRef.GeographicToEngine(_cameraRef.Position)).Item1, 
+                CameraMovement.Instance.GetCameraCentralTile(_mapRef.GeographicToEngine(_cameraRef.Position)).Item2
+            );
+
+            //Get current texture tile
+            _gisPostToPixel = ShaderTextureTilingController.Instance.tiles[cameraInTextureTileNumber].transform.GetChild(0).GetComponent<GisPosToPixel>();
             _pointInUV = _gisPostToPixel.gisPosToPixelMethod(_gpsPosition);
             _shaderImage = _gisPostToPixel.gameObject.GetComponent<GISPosShader>();
 
-            if (_lastPosition != null && _lastDataTimestamp != null && (DateTime.UtcNow - _lastDataTimestamp).TotalSeconds < 15)
+            if (_lastPosition != null && _lastGotGPSTime != null && (DateTime.UtcNow - _lastGotGPSTime).TotalSeconds < 15)
             {
                 if (!_mapRef.HasSpatialReference())
                 {
                     Debug.Log("Waiting for ArcGISMapComponent to have a valid spatial reference...");
-                    _gisPostToPixel = ShaderTextureTilingController.Instance.tiles[new Vector2(CameraMovement.Instance.GetCameraCentralTile(_mapRef.GeographicToEngine(_cameraRef.Position)).Item1, CameraMovement.Instance.GetCameraCentralTile(_mapRef.GeographicToEngine(_cameraRef.Position)).Item2)].transform.GetChild(0).GetComponent<GisPosToPixel>();
                 }
 
                 //Draw line between _lastPosition and current gps position (Designed for losing gps for short period of time)
@@ -239,17 +211,16 @@ public class LocationService : MonoBehaviour
                 _shaderImage.updateLineInTexture(_pointInUV, _lastPointInUV);
             }
             _lastPosition = _gpsPosition;
-            _lastDataTimestamp = DateTime.UtcNow;
+            _lastGotGPSTime = DateTime.UtcNow;
         }
-        else  //Lost GPS 
+        else  
         {
             Debug.Log("Lose GPS data");
-            
         }
 
-        if (_lastDataTimestamp!= null && ((DateTime.UtcNow - _lastDataTimestamp).TotalSeconds >= 15) ) //Lost GPS for more than 15f
+        if (_lastGotGPSTime!= null && ((DateTime.UtcNow - _lastGotGPSTime).TotalSeconds >= 15) ) //Lost GPS for more than 15f
         {
-            testUIGPS.sprite = _globeImage[0];
+            _testUIGPS.sprite = _globeImage[0];
             Debug.Log("Time over 15sec");
         }
 
@@ -264,39 +235,39 @@ public class LocationService : MonoBehaviour
             if (!_mapRef.HasSpatialReference())
             {
                 Debug.Log("Waiting for ArcGISMapComponent to have a valid spatial reference...");
-                _gisPostToPixel = ShaderTextureTilingController.Instance.tiles[new Vector2(CameraMovement.Instance.GetCameraCentralTile(_mapRef.GeographicToEngine(_cameraRef.Position)).Item1, CameraMovement.Instance.GetCameraCentralTile(_mapRef.GeographicToEngine(_cameraRef.Position)).Item2)].transform.GetChild(0).GetComponent<GisPosToPixel>();
-                //_pointInUV = _gisPostToPixel.gisPosToPixelMethodOffline(_mapRef.GeographicToEngine(_gpsPosition));
-                //_shaderImage = _gisPostToPixel.gameObject.GetComponent<GISPosShader>();
-                //_shaderImage.updatePositionInTexture(_pointInUV);
-                //yield return null;
-
             }
 
-            _gisPostToPixel = ShaderTextureTilingController.Instance.tiles[new Vector2(CameraMovement.Instance.GetCameraCentralTile(_mapRef.GeographicToEngine(_cameraRef.Position)).Item1, CameraMovement.Instance.GetCameraCentralTile(_mapRef.GeographicToEngine(_cameraRef.Position)).Item2)].transform.GetChild(0).GetComponent<GisPosToPixel>();
+            Vector2 cameraInTextureTileNumber = new Vector2(
+                CameraMovement.Instance.GetCameraCentralTile(_mapRef.GeographicToEngine(_cameraRef.Position)).Item1,
+                CameraMovement.Instance.GetCameraCentralTile(_mapRef.GeographicToEngine(_cameraRef.Position)).Item2
+            );
+
+            _gisPostToPixel = ShaderTextureTilingController.Instance.tiles[cameraInTextureTileNumber].transform.GetChild(0).GetComponent<GisPosToPixel>();
             _pointInUV = _gisPostToPixel.gisPosToPixelMethod(_gpsPosition);
             _shaderImage = _gisPostToPixel.gameObject.GetComponent<GISPosShader>();
             _shaderImage.updatePositionInTexture(_pointInUV);
             
         }
-        
-        
+        _lastCheckGPSTime = DateTime.UtcNow;
+
+
     }
 
     private void CheckInternet()
     {
         if (Application.internetReachability == NetworkReachability.NotReachable)
         {
-            testUIWifi.color = Color.red;
+            _testUIWifi.color = Color.red;
             
         }
         else if (Application.internetReachability != NetworkReachability.NotReachable)
         {
-            testUIWifi.color = Color.green;
-            if (!_mapRef.HasSpatialReference() && isFirstTimeReload)
+            _testUIWifi.color = Color.green;
+            if (!_mapRef.HasSpatialReference() && _isFirstTimeReload)
             {
                 Debug.Log("try initialising map");
                 StartCoroutine(ReInitialiseMap());
-                isFirstTimeReload = false;
+                _isFirstTimeReload = false;
             }
         }
     }
@@ -329,10 +300,9 @@ public class LocationService : MonoBehaviour
     /// <returns></returns>
     IEnumerator ReInitialiseMap()
     {
-        yield return new WaitForSecondsRealtime(1);
-        _mapRef.enabled = false;
-        yield return new WaitForSecondsRealtime(1);
-        _mapRef.enabled = true;
+       _mapRef.enabled = false;
+       yield return new WaitForSecondsRealtime(1);
+       _mapRef.enabled = true;
     }
 
 }
